ed9f0c0cb10b3121bac721ca39be7bb546e62c22
limjae <limjae96@gmail.com>
2022-05-06 18:23:35 +0900

    Feat: creating user services and repository

--- HanghaeProject/src/main/java/com/team7/project/user/repository/UserRepository.java
+++ HanghaeProject/src/main/java/com/team7/project/user/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.team7.project.user.repository;
 
-public class UserRepository {
+import com.team7.project.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String userEmail);
+    Optional<User> findByNickName(String userName);
 }
---
+++ HanghaeProject/src/main/java/com/team7/project/user/service/UserProfileService.java
@@ -0,0 +1,4 @@
+package com.team7.project.user.service;
+
+public class UserProfileService {
+}
