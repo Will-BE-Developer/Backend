a68ab0fa8a073ffbbf9f52d829551e986b14113d
sesangmak@gmail.com <sesangmak@gmail.com>
2022-05-08 00:11:18 +0900

    Feat: warpping by 'comment' in response json

--- HanghaeProject/src/main/java/com/team7/project/comments/dto/CommentResponseDto.java
+++ HanghaeProject/src/main/java/com/team7/project/comments/dto/CommentResponseDto.java
@@ -15,39 +15,38 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
-    //private Comment comment;
-    private Long id;
-    @JsonIgnore
-    private User userOrigin;
-    private ResponseUser user;
-    private String contents;
-    private Boolean isMine;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
+    private ResponseComment comment;
 
-    public CommentResponseDto(Comment comment, Boolean isMine) {
-        //this.comment = comment;
-        this.id = comment.getId();
-        //this.user = comment.getUser();
-        this.userOrigin = comment.getUser();
-        this.user = new ResponseUser(
-                userOrigin.getId(),
-                userOrigin.getNickname(),
-                userOrigin.getGithubLink(),
-                userOrigin.getProfileImageUrl(),
-                userOrigin.getIntroduce());
-        this.contents = comment.getContents();
-        this.createdAt = comment.getCreatedAt();
-        this.modifiedAt = comment.getModifiedAt();
-        this.isMine = isMine;
+    public CommentResponseDto(Comment comment, Boolean isMine){
+        this.comment = new ResponseComment(comment, isMine);
     }
 
-//    public void setComment(Comment comment) {
-//        this.comment = comment;
-//    }
-//    public void setMine(Boolean mine) {
-//        isMine = mine;
-//    }
+    @Getter
+    public class ResponseComment{
+        private Long id;
+        @JsonIgnore
+        private User userOrigin;
+        private ResponseUser user;
+        private String contents;
+        private Boolean isMine;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+        
+        public ResponseComment(Comment comment, Boolean isMine) {
+            this.id = comment.getId();
+            this.userOrigin = comment.getUser();
+            this.user = new ResponseUser(
+                    userOrigin.getId(),
+                    userOrigin.getNickname(),
+                    userOrigin.getGithubLink(),
+                    userOrigin.getProfileImageUrl(),
+                    userOrigin.getIntroduce());
+            this.contents = comment.getContents();
+            this.createdAt = comment.getCreatedAt();
+            this.modifiedAt = comment.getModifiedAt();
+            this.isMine = isMine;
+        }
+    }
     @Getter
     @AllArgsConstructor
     public class ResponseUser{
