3999feca2400aafac5f42d04f35ec88238cbd247
limjae <limjae96@gmail.com>
2022-05-09 09:40:53 +0900

    Refactor: CategoryController

--- HanghaeProject/src/main/java/com/team7/project/category/controller/CategoryController.java
+++ HanghaeProject/src/main/java/com/team7/project/category/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.team7.project.category.controller;
 
 import com.team7.project.category.dto.CategoryResponseDto;
+import com.team7.project.category.model.CategoryEnum;
 import com.team7.project.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,9 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/api/categories")
-    public CategoryResponseDto getAllCategories() {
-        return new CategoryResponseDto(categoryService.getCategoryNames());
+    public ResponseEntity<CategoryResponseDto> getAllCategories() {
+        List<CategoryEnum> categoryEnumList = categoryService.getCategoryNames();
+
+        return new ResponseEntity<>(new CategoryResponseDto(categoryEnumList), HttpStatus.OK);
     }
 }
