e12798f4cd3ca7b441116c6dda10dad55470b416
limjae <limjae96@gmail.com>
2022-05-11 11:49:16 +0900

    Feat : batch test

--- HanghaeProject/src/main/java/com/team7/project/ProjectApplication.java
+++ HanghaeProject/src/main/java/com/team7/project/ProjectApplication.java
@@ -1,12 +1,16 @@
 package com.team7.project;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+@EnableBatchProcessing
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class ProjectApplication {
