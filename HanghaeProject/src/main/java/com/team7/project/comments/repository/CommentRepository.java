fc366a6d74445f5064648f4a838b8806a99cbf65
sesangmak@gmail.com <sesangmak@gmail.com>
2022-05-11 15:15:00 +0900

    Feat: made comment with login user, page

--- HanghaeProject/src/main/java/com/team7/project/comments/repository/CommentRepository.java
+++ HanghaeProject/src/main/java/com/team7/project/comments/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.team7.project.comments.repository;
 
 import com.team7.project.comments.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +13,13 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByInterviewId(Long interviewId);
 
+    @Query(value = "SELECT * FROM comment WHERE interview_id = :interviewId  and root_name = :rootName LIMIT :per OFFSET :offset", nativeQuery = true)
+    //List<Comment> findAllByInterviewIdAndRootName(@Param("interviewId") Long interviewId, @Param("rootName") String rootName);
+    List<Comment> findAllByInterviewIdAndRootName(@Param("interviewId") Long interviewId, @Param("rootName") String rootName,
+                                                  @Param("per") int per,  @Param("offset") int offset);
+    //Page<Comment> findAllByInterviewIdAndRootName(@Param("interviewId") Long interviewId, @Param("rootName") String rootName, Pageable pageable); //추후 적용 테스트
+
     @Query(value = "SELECT * FROM comment WHERE interview_id = :interviewId  and root_name = :rootName", nativeQuery = true)
-    List<Comment> findAllByInterviewIdAndRootName(@Param("interviewId") Long interviewId, @Param("rootName") String rootName);
+    List<Comment> findAllByInterviewIdAndRootNameNest(@Param("interviewId") Long interviewId, @Param("rootName") String rootName);
 }
 
